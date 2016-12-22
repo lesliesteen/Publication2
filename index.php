91800fe53656252e41e49fe2c0d7103f1b5e9353
lesliesteen <Leslie@MacBook-Pro-van-Leslie.local>
2016-12-22 17:05:40 +0100

    master versie

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+<body>
+<?php 
+$now = new DateTime();
+echo $now->format('F');
+echo $now->format('d');
+echo $now->format('l');
+?>
+    
+    
+</body>
+    
+</html>
